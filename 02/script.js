0ff9ac909dca867e31969395eddfb27143b54931
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-06-12 17:50:22 +0500

    Today Files

--- 02/script.js
+++ 02/script.js
@@ -2,8 +2,43 @@
 //Var 
 var age = 20;
 
-console.log(age);
-
 var num2 = 30;
 var num3 = 40;
-consol.log(num2 + num3); 
+console.log(num2 + num3); 
+console.log(age);
+
+//var -> Globel
+if(true){
+    var age = 30;
+console.log(age);
+}
+
+//Let
+if(true){
+    let a = 20;
+    console.log(a);
+}
+
+
+//const 
+//const -> local
+//const -> values re-assign not allowed
+ const age1 = 20;
+
+ console.log(age1 + 10);
+
+
+
+ //Revision
+
+ //var -> global
+ //Khi bhi assign kr sakte hai
+ //Re-assign allowed
+
+ //let -> local
+ //Re-assign allowed
+ //Acces sirf khud ky block me hota hai
+
+ //const -> local
+//Re-assign not allowed
+//Acces sirf khud ky block me hota hai
