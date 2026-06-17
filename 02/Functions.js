f34643db93c89e7f9fbc95c775f91aa6dea1c23c
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-06-17 16:18:41 +0500

    Functions

---
+++ 02/Functions.js
@@ -0,0 +1,19 @@
+
+
+// function showMessage() {
+//   let message = "Hello, I'm JavaScript!"; // local variable
+
+  
+//   alert( message );
+// }
+
+// showMessage(); // Hello, I'm JavaScript!
+
+
+let userName = 'John';
+
+function showMessage() {
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+showMessage(); // Hello, John
