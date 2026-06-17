368919f9fa0cd2dedd5143e9bcd7984f05163ba8
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-06-17 16:29:05 +0500

    Initial commit

--- 02/Functions.js
+++ 02/Functions.js
@@ -1,17 +1,14 @@
 
-
-// function showMessage() {
-//   let message = "Hello, I'm JavaScript!"; // local variable
-
-  
-//   alert( message );
-// }
-
-// showMessage(); // Hello, I'm JavaScript!
+//First Example...
+function showMessage() {
+  let message = "Hello, I'm JavaScript!"; // local variable 
+  alert( message );
+}
+showMessage(); // Hello, I'm JavaScript!
 
 
+//Second Example...
 let userName = 'John';
-
 function showMessage() {
   let message = 'Hello, ' + userName;
   alert(message);
