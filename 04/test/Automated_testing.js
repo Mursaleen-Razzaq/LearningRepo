b7ec45014eadb76fd8700fe67abc09cd41656d01
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-06-19 18:38:11 +0500

    Automated Testing

---
+++ 04/test/Automated_testing.js
@@ -0,0 +1,22 @@
+// const assert = require('assert');
+
+// describe("pow", function() {
+//   it("raises to n-th power", function() {
+//     assert.equal(2 ** 3, 8);
+//   });
+// });
+
+
+
+// function add(a, b) {
+//     return a + b;
+// }
+
+// function subtract(a, b) {
+//     return a - b;
+// }
+
+// if (typeof module !== "undefined" && module.exports) {
+//     console.log("Testing")
+//     module.exports = { add, subtract };
+// }
