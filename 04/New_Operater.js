5a66aaaee43ad61fa5766f36d67f6f5527b22dac
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-06-22 19:08:24 +0500

    New_Operater

---
+++ 04/New_Operater.js
@@ -0,0 +1,22 @@
+function User(name) {
+  if (!new.target) { // if you run me without new
+    return new User(name); // ...I will add new for you
+  }
+
+  this.name = name;
+}
+
+let john = User("John"); // redirects call to new User
+alert(john.name); // John
+
+//function User(name) {
+  this.name = name;
+
+  this.sayHi = function() {
+    alert( "My name is: " + this.name );
+  };
+}
+
+let john = new User("John");
+
+john.sayHi(); // My name is: John
