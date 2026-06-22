445ec8b3345204c69d7abfe0cbd0ba54eb2ecbd6
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-06-22 19:11:03 +0500

    Array

---
+++ 05/Array.js
@@ -0,0 +1,12 @@
+//Arrays...
+const myArr = [0, 2, 3, 4, 5];
+// console.log(myArr[2]);
+const myArr2 = new Array(2, 4, 6, 8);
+// console.log(myArr2[0]);
+
+//Array Methods..
+// myArr.push(6, 7);
+// myArr.pop();
+myArr.unshift(1);
+myArr.shift();
+console.log(myArr);
