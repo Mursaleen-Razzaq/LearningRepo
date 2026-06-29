59d6071447f703acb1806cc346c429957673b841
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-06-29 18:54:53 +0500

    Iterables

---
+++ 05/Iterables.js
@@ -0,0 +1,12 @@
+// Iterables in JavaScript
+
+// 1. For...of loop with arrays
+const arr = [1, 2, 3, 4, 5];
+console.log('For...of with array:');
+for (const value of arr) {
+  console.log(value);
+}
+
+//
+const myArr = ['Apple', 'Grapes', 'Oranges', 'Bhindi'];
+console.log("My array is ", myArr);
