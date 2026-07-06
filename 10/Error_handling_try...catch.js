7293f11d9c8e3961155266738ba739138bd90d88
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-07-06 19:04:23 +0500

    Error_handling_try...catch

---
+++ 10/Error_handling_try...catch.js
@@ -0,0 +1,27 @@
+
+setTimeout(() =>{
+    console.log("Hacking WIFI.... Please wait...");
+    
+}, 1000)
+
+  try {
+    console.log(Rashid);
+ }
+ catch(error){
+    console.log("Balle Balle" + error); 
+ }
+
+setTimeout(() =>{
+    console.log("Fetching Username and Password.... Please wait...");
+    
+}, 2000)
+
+setTimeout(() =>{
+    console.log("Hacking Rashid facebook id.... Please wait...");
+    
+}, 3000)
+
+setTimeout(() =>{
+    console.log("Username and Password of Rashid (+923064448000) fetched.... Please wait...");
+    
+}, 4000)
