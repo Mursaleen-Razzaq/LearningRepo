631aa40ed8f88cfd2a85f0aa3ceefccd0a5963da
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-07-08 19:00:07 +0500

    Import in module 2

---
+++ 13/module2.js
@@ -0,0 +1,15 @@
+
+
+import hello from "./module1.js";
+console.log(hello("Mursaleen"));
+
+import { add, subtract, divide, mul, name } from "./module1.js";
+
+console.log(add(10, 20));
+
+console.log(add(3, 4));
+console.log(subtract(6, 9));
+console.log(divide(3, 4));
+console.log(mul(2, 7));
+// console.log(name("Mursaleen"));
+
