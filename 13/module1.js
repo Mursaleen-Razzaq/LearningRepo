36ccd291e9a0e99bffe0ed04e67d836076e41254
Mursaleen Razzaq <mursaleenrazzaq101@gmail.com>
2026-07-08 18:59:33 +0500

    Export in module 1

---
+++ 13/module1.js
@@ -0,0 +1,30 @@
+
+//
+export default function hello(name) {
+    return(`Hello ${name}`);
+}
+
+// //
+export function add(a, b) {
+    return a + b;
+}
+export function subtract(c, d){
+    return(c - d);
+      
+}
+
+export function divide(i, j){
+    return(i / j);
+   
+    
+}
+
+export function name(){
+    return(`Hello ${name}`);
+    
+}
+
+export function mul(k, l){
+    return(k * l);
+    
+}
